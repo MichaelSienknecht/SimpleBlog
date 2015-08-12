02d48e1261c6ae931e8cb67b8efbdec9989bbb7b
unknown <praktikant@NUC1.B-soft.lan>
2015-08-12 13:07:05 +0200

    Adding scripts to the bundle.

--- SimpleBlog/App_Start/BundleConfig.cs
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -6,9 +6,25 @@ namespace SimpleBlog.App_Start
     {
         public static void RegisterBundels(BundleCollection bundels)
         {
-            bundels.Add(new StyleBundle("~/admin/styles").Include("~/content/bootstrap.css").Include("~/content/styles/admin.css"));
+            bundels.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
 
-            bundels.Add(new StyleBundle("~/Styles").Include("~/Content/bootstrap.css").Include("~/Content/Styles/Site.css"));
+            bundels.Add(new StyleBundle("~/Styles")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/Styles/Site.css"));
+
+            bundels.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.4.js")
+                .Include("~/Scripts/jquery.validate.min.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.min.js")
+                .Include("~/Scripts/bootstrap.js"));
+
+            bundels.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.4.js")
+                .Include("~/Scripts/jquery.validate.min.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.min.js")
+                .Include("~/Scripts/bootstrap.js"));
         }
     }
 }
